c6eceff7c332eeaa1716383ca7bb38246a280655
marlonszpak <marloszpaik@gmail.com>
2024-01-18 22:03:50 -0300

    Criando autenticações

--- src/main/java/com/example/ControleEstoque/repositories/UsuarioRepository.java
+++ src/main/java/com/example/ControleEstoque/repositories/UsuarioRepository.java
@@ -3,7 +3,11 @@ package com.example.ControleEstoque.repositories;
 import com.example.ControleEstoque.models.Usuario;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    @Transactional
+    Usuario findByUsername(String username);
 }
--- src/main/java/com/example/ControleEstoque/services/UserDetailsServiceImp.java
+++ src/main/java/com/example/ControleEstoque/services/UserDetailsServiceImp.java
@@ -1,14 +1,27 @@
 package com.example.ControleEstoque.services;
 
+import com.example.ControleEstoque.models.Usuario;
+import com.example.ControleEstoque.repositories.UsuarioRepository;
+import com.example.ControleEstoque.security.UserSpringSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Usuario usuario = usuarioRepository.findByUsername(username);
+        if (Objects.isNull(usuario));
+            throw new UsernameNotFoundException("Usuario não encontrado: " + username);
+        return new UserSpringSecurity(usuario.getId(),usuario.getNome(),usuario.getSenha(),usuario.getProfiles());
     }
 }
