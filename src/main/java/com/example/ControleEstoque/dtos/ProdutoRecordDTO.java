9e541a0dd6a0d8b694a4b178f91bcc3054d28b0d
marlonszpak <marloszpaik@gmail.com>
2024-01-21 21:59:03 -0300

    Criando autenticações

--- src/main/java/com/example/ControleEstoque/dtos/ProdutoRecordDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.ControleEstoque.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record ProdutoRecordDTO(@NotBlank String nome, @NotBlank String endereco, @NotNull Integer quantidade) {
-}
--- src/main/java/com/example/ControleEstoque/dtos/UsuarioRecordDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.ControleEstoque.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UsuarioRecordDTO(@NotBlank String nome, @NotBlank String email, @NotBlank String senha) {
-}
