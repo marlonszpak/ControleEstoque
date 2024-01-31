738904c9db1fde8792491f3c531bd798c454254f
marlonszpak <marloszpaik@gmail.com>
2024-01-31 20:43:32 -0300

    Criando os User service

---
+++ src/main/java/com/example/ControleEstoque/services/UserService.java
@@ -0,0 +1,48 @@
+package com.example.ControleEstoque.services;
+
+import com.example.ControleEstoque.models.User;
+import com.example.ControleEstoque.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuario não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas!");
+        }
+    }
+
+
+}
