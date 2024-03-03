b39a0346a062dd9647a93c366d6614445b887d88
marlonszpak <marloszpaik@gmail.com>
2024-03-03 20:38:12 -0300

    Criando um exception handler para tratar os erros do projeto

--- src/main/java/com/example/ControleEstoque/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/ControleEstoque/exceptions/GlobalExceptionHandler.java
@@ -4,24 +4,21 @@ import java.io.IOException;
 import java.nio.file.AccessDeniedException;
 
 import javax.naming.AuthenticationException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolationException;
+
+import com.example.ControleEstoque.services.exceptions.AuthorizationException;
+import com.example.ControleEstoque.services.exceptions.DataBindingViolationException;
+import com.example.ControleEstoque.services.exceptions.ObjectNotFoundException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.ConstraintViolationException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,6 +27,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.example.ControleEstoque.services.exceptions.ObjectNotFoundException;
 
 
 
--- src/main/java/com/example/ControleEstoque/services/UserService.java
+++ src/main/java/com/example/ControleEstoque/services/UserService.java
@@ -2,6 +2,7 @@ package com.example.ControleEstoque.services;
 
 import com.example.ControleEstoque.models.User;
 import com.example.ControleEstoque.repositories.UserRepository;
+import com.example.ControleEstoque.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +17,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow(() -> new RuntimeException(
+        return user.orElseThrow(() -> new ObjectNotFoundException(
                 "Usuario não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
