cf672ea56ee3b05fed2a6fe46d6588f237884a90
marlonszpak <marloszpaik@gmail.com>
2024-03-10 18:06:36 -0300

    Refazendo a parte de security

--- src/main/java/com/example/ControleEstoque/models/User.java
+++ src/main/java/com/example/ControleEstoque/models/User.java
@@ -2,9 +2,7 @@ package com.example.ControleEstoque.models;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import com.example.ControleEstoque.models.enums.ProfileEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -47,13 +45,12 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Set<Integer> profiles = new HashSet<>();
 
-    public Set<ProfileEnum> getProfiles() {
-        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
-    }
-
-    public void addProfile(ProfileEnum profileEnum) {
-        this.profiles.add(profileEnum.getCode());
-    }
-
+//    public Set<ProfileEnum> getProfiles() {
+//        return this.profiles.stream().map(x -> ProfileEnum.toEnum(x)).collect(Collectors.toSet());
+//    }
+//
+//    public void addProfile(ProfileEnum profileEnum) {
+//        this.profiles.add(profileEnum.getCode());
+//    }
 
 }
--- src/main/java/com/example/ControleEstoque/services/UserService.java
+++ src/main/java/com/example/ControleEstoque/services/UserService.java
@@ -1,30 +1,22 @@
 package com.example.ControleEstoque.services;
 
 import com.example.ControleEstoque.models.User;
-import com.example.ControleEstoque.models.enums.ProfileEnum;
 import com.example.ControleEstoque.repositories.UserRepository;
-import com.example.ControleEstoque.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class UserService {
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow(() -> new ObjectNotFoundException(
+        return user.orElseThrow(() -> new RuntimeException(
                 "Usuario não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
@@ -32,8 +24,6 @@ public class UserService {
     @Transactional
     public User create(User obj){
         obj.setId(null);
-        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
-        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -42,7 +32,6 @@ public class UserService {
     public User update(User obj){
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
-        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
